a714a1709bac0083dfe025ecab2d750c901470fd
Yathin-KN <103889683+Yathin-KN@users.noreply.github.com>
2022-11-07 19:50:42 +0530

    Update frog-1(BFS Approach).cpp

--- Trees_and_Graph/frog-1(BFS Approach).cpp
+++ Trees_and_Graph/frog-1(BFS Approach).cpp
@@ -1,3 +1,25 @@
+// *** warning **** -> this is not the correct approach to solve the problem <- *** warning ****
+// The solution i tried here is purely experimental:
+//The question states that there are N tiles and a frog which is in the 0th tile has to reach the Nth tile in minimum number of jumps with a restriction that 
+//if suppose the frog is on ith tile it can jump to either i+1th tile or i+2 th tile
+
+//well since i had learned BFS recently and which is an algo to find minimum path at many problems eventhough the approach isn't needed here i tried to implement it
+//so the code here assumes the frog is already at tile 6 (Nth tile here) thus the frog can havee arrived here from either N-1th or N-2th tile
+//so using the same concept for both N-1th as well as N-2 th tile I imagined a probability tree ( as i like to call ) which sorta look like this:
+//                    6            
+//              /          \
+//             5            4
+//          /    \        /   \
+//         4      3      3     2
+//        / \    / \    / \     \
+//       3   2  1   2  2   1     1
+//      / \   \      \ \
+//     2   1   1      1 1
+//    /
+//   1
+// if we apply a bfs traversal the level at which the tile 1 is apprached one becomes the shortest path as shown by the arrows the path
+// to be followed by the from in ordered to reach thr 6th tile is either:
+// 1->3->5->6   1->2->4->6  1->3->4->6 i.e minimum of three jumps is required :) yeah preeeety confusing sol but yeah makes your hold on BFS preety strong:
 #include<bits/stdc++.h>
 using namespace std;
 struct Node{
